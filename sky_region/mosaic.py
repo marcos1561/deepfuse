b2211090fe75c1ef701de0b5b6eb92646e95c60f
Marcos Pasa <marcospasa130@gmail.com>
2021-10-22 17:01:59 -0300

    changed the file structure

--- sky_region/mosaic.py
+++ sky_region/mosaic.py
@@ -6,37 +6,52 @@ os = func.os
 cd = os.path.join(os.getcwd())
 print(cd)
 
-# Download sky region #####################
+# User configuration #####################
+
+# main directory that will containg everything
+main_dir = os.path.join(cd, "sky_region/test_region")
+os.makedirs(main_dir, exist_ok=True)
+
 # Configurations for the sky region to download 
 central_pos = (210.8002, 54.3478) # degre, degree
 height = 0.7/(60*60)*(256*2) # degree
 length = 0.7/(60*60)*(256*4) # degree
 ps = 0.7 # arcsec
 
-file_path = os.path.join(cd, "ims_to_downlaod.csv")
-mosaic_im_dir = os.path.join(cd, "mosaic_ims")
-mosaic_ids_file = os.path.join(cd, "mosaic_ids.csv")
+# Cutouts configurations 3 
+cutout_size = 200
+overlap_percentage = 0.1
+###########################################
 
+
+# Paths ##################################
+
+file_path = os.path.join(main_dir, "ims_to_downlaod.csv") # file with the images to download
+mosaic_ids_file = os.path.join(main_dir, "mosaic_ids.csv") # file informing wich images were downloaded correctly
+mosaic_im_dir = os.path.join(main_dir, "mosaic_ims") # directory tha will contain the images downloaded
+mosaic_seg_dir = os.path.join(main_dir, "mosaic_segments") # directory with mosaic segments
+###########################################
+
+
+# Download sky region #####################
 
 # Generating file with the images to download
 func.generate_sky_region_file(central_pos, length, height, ps, file_path)
 
 # Downloading the images
+
 # TODO: Make sure every image was downloaded
 fetch.fetch(file_path, mosaic_im_dir, mosaic_ids_file, "ls-dr9")
 ###########################################
 
 
 # Generating mosaic from the images downloaded ###
+
 array, footprint, wcs_out, shape_out = func.generate_mosaic(mosaic_im_dir, 0)
 ###########################################
 
 
 # Generate segmets from de mosaic created ###
-# Configurations 
-cutout_size = 200
-overlap_percentage = 0.1
-mosaic_seg_dir = os.path.join(cd, "mosaic_segments")
 
 # Creating segments
 nrows, ncols = func.generae_mosaic_segments(array, wcs_out, shape_out, mosaic_seg_dir, cutout_size, overlap_percentage)
@@ -44,6 +59,7 @@ nrows, ncols = func.generae_mosaic_segments(array, wcs_out, shape_out, mosaic_se
 
 
 # Visualing mosaic ###
+
 # All mosaic
 func.visualize_mosaic(array, footprint)
 
@@ -52,4 +68,3 @@ func.visualize_mosaic_segments(mosaic_seg_dir, nrows, ncols, array, sharexy=Fals
 
 func.plt.show()
 ###########################################
-
